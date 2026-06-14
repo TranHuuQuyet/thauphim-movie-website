329d40429d20bd59877803a87555d7efe9aff9df
Tran Huu Quyet <huuquyet120406@gmail.com>
2026-06-14 22:54:54 +0700

    sua footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,22 +1,19 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 <footer class="site-footer">
     <div class="footer-container">
-        
-        <div class="footer-banner">
-            <span class="banner-badge"><i class="fa-solid fa-star"></i> Hoàng Sa & Trường Sa là của Việt Nam!</span>
-        </div>
+
 
         <div class="footer-main-row">
             <div class="footer-brand">
                 <div class="footer-logo">
-                    <i class="fa-regular fa-circle-play logo-icon"></i>
+                    <img class="footer-img" src="assets/images/favicon.png" alt="">
                     <div class="logo-text">
-                        <h2>RoPhim</h2>
-                        <span>Phim hay cả rổ</span>
+                        <h2>Thau<strong>Phim</strong></h2>
+                        <span>Phim hay cả thau</span>
                     </div>
                 </div>
             </div>
-            
+
             <div class="footer-socials">
                 <a href="#" class="social-item telegram" title="Telegram"><i class="fa-brands fa-telegram"></i></a>
                 <a href="#" class="social-item discord" title="Discord"><i class="fa-brands fa-discord"></i></a>
@@ -52,10 +49,14 @@
 
         <div class="footer-bottom">
             <p class="footer-description">
-                <strong>RoPhim</strong> – Phim hay cả rổ - Trang xem phim online chất lượng cao miễn phí Vietsub, thuyết minh, lồng tiếng full HD. Kho phim mới khổng lồ, phim chiếu rạp, phim bộ, phim lẻ từ nhiều quốc gia như Việt Nam, Hàn Quốc, Trung Quốc, Thái Lan, Nhật Bản, Âu Mỹ... đa dạng thể loại. Khám phá nền tảng phim trực tuyến hay nhất 2026 chất lượng 4K!
+                <strong>ThauPhim</strong> – Phim hay cả thau - Trang xem phim online chất lượng cao miễn phí Vietsub,
+                thuyết
+                minh, lồng tiếng full HD. Kho phim mới khổng lồ, phim chiếu rạp, phim bộ, phim lẻ từ nhiều quốc gia như
+                Việt Nam, Hàn Quốc, Trung Quốc, Thái Lan, Nhật Bản, Âu Mỹ... đa dạng thể loại. Khám phá nền tảng phim
+                trực tuyến hay nhất 2026 chất lượng 4K!
             </p>
             <div class="footer-copyright">
-                &copy; <?php echo date("Y"); ?> <span>RoPhim</span>. All rights reserved.
+                &copy; <?php echo date("Y"); ?> <span>ThauPhim</span>. All rights reserved.
             </div>
         </div>
 
