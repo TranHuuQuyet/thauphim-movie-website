3ffb2741539336dac4721b3600a1ef3f8e8c7d05
Tran Huu Quyet <huuquyet120406@gmail.com>
2026-06-18 17:52:23 +0700

    login form

--- includes/auth_modal.php
+++ includes/auth_modal.php
@@ -1,23 +1,26 @@
 <div class="auth-modal" id="authModal">
-    <!-- <div id="login-box" class="modal"> -->
-    <button type="button" id="closeAuth" aria-label="Dong form dang nhap">
-        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
-            <path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z" />
-        </svg>
-    </button>
-    <form action="#" method="post">
-        <!-- <div class="login-form"> -->
-        <h2 class="gradient-text">Thau Phim</h2>
-        <h2>Đăng nhập</h2>
-        <p>Bạn chưa có tài khoản,<a href=""><span>Đăng ký ngay!</span></a></p>
-        <label class="sr-only" for="loginUsername">Ten dang nhap</label>
-        <input id="loginUsername" type="text" placeholder="username" name="username" autocomplete="username" />
-        <label class="sr-only" for="loginPassword">Mat khau</label>
-        <input id="loginPassword" type="password" placeholder="password" name="password"
-            autocomplete="current-password" />
-        <button type="submit">Đăng nhập</button>
-        <p>QUÊN MẬT KHẨU?</p>
-        <!-- </div> -->
-    </form>
-    <!-- </div> -->
+    <div class="container">
+        <div class="img-ad">
+            <img src="assets/images/img_login.jpg" alt="">
+        </div>
+        <div class="modal">
+            <button type="button" id="closeAuth" aria-label="Dong form dang nhap">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                    fill="#e3e3e3">
+                    <path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z" />
+                </svg>
+            </button>
+            <form action="#" method="post">
+                <h2 class="gradient-text">Thau Phim</h2>
+                <p>Bạn chưa có tài khoản,<a href=""><span>Đăng ký ngay.</span></a></p>
+                <label class="sr-only" for="loginUsername">Ten dang nhap</label>
+                <input id="loginUsername" type="text" placeholder="username" name="username" autocomplete="username" />
+                <label class="sr-only" for="loginPassword">Mat khau</label>
+                <input id="loginPassword" type="password" placeholder="password" name="password"
+                    autocomplete="current-password" />
+                <button type="submit">Đăng nhập</button>
+                <p>QUÊN MẬT KHẨU?</p>
+            </form>
+        </div>
+    </div>
 </div>
