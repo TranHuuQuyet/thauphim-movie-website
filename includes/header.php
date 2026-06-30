0dfdadabd092285938a63a59feedaea713657221
Hoang Thanh Dat <thanhdathoang26082006@gmail.com>
2026-07-01 01:08:18 +0700

    S1

--- includes/header.php
+++ includes/header.php
@@ -85,7 +85,7 @@ $notificationUnreadCount = count(array_filter(
                     <a href="/pages/actor.php">Diễn viên</a>
                 </nav>
 
-                <form class="search-form" action="/thauphim-movie-website/pages/browse.php" method="get" role="search">
+                <form class="search-form" action="/pages/browse.php" method="get" role="search">
                     <label class="sr-only" for="header-search">Tìm phim, diễn viên</label>
                     <i class="fa-solid fa-magnifying-glass" aria-hidden="true"></i>
                     <input id="header-search" name="q" type="search" placeholder="Tìm phim..." autocomplete="off"
